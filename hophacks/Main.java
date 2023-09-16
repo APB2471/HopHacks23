415f9fb9a51f6d9262a5890f394d182e7f6abc55
Andrew Bush <apb2471@cs.rit.edu>
2023-09-16 17:23:09 -0400

    feat: Added ascii art

--- hophacks/Main.java
+++ hophacks/Main.java
@@ -7,8 +7,27 @@ import java.util.Scanner;
  *
  */
 public class Main {
+    Profile current;
     public static void main(String[] args) {
         ArrayList<Profile> profiles = new ArrayList<>();
+        System.out.println("                            _\n" +
+                        "                          .' `'.__\n" +
+                        "                         /      \\ `'\"-,\n" +
+                        "        .-''''--...__..-/ .     |      \\\n" +
+                        "      .'               ; :'     '.  a   |\n" +
+                        "     /                 | :.       \\     =\\\n" +
+                        "    ;                   \\':.      /  ,-.__;.-;`\n" +
+                        "   /|     .              '--._   /-.7`._..-;`\n" +
+                        "  ; |       '                |`-'      \\  =|\n" +
+                        "  |/\\        .   -' /     /  ;         |  =/\n" +
+                        "  (( ;.       ,_  .:|     | /     /\\   | =|\n" +
+                        "   ) / `\\     | `\"\"`;     / |    | /   / =/\n" +
+                        "     | ::|    |      \\    \\ \\    \\ `--' =/\n" +
+                        "    /  '/\\    /       )    |/     `-...-`\n" +
+                        "   /    | |  `\\    /-'    /;\n" +
+                        "   \\  ,,/ |    \\   D    .'  \\\n" +
+                        "jgs `\"\"`   \\  nnh  D_.-'L__nnh\n" +
+                        "            `\"\"\"`");
         System.out.println("TrialHelp 1.0");
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -16,6 +35,7 @@ public class Main {
             System.out.println("1: Quit");
             System.out.println("2: FAQs");
             System.out.println("3: Create profile");
+            System.out.println("4: Log in");
             int choice = sc.nextInt();
             if (choice == 1) {
                 break;
@@ -27,6 +47,14 @@ public class Main {
                 Profile newProfile = ap.NewProfile();
                 System.out.println(newProfile.getUsername());
                 profiles.add(newProfile);
+                System.out.println("Your new profile" + newProfile.getUsername() +
+                        "has been created");
+            } else if (choice == 4) {
+                System.out.println("Enter your username: ");
+                //TODO: Select Profile from entered username
+            } else {
+                System.out.println("Invalid selection, please type the" +
+                        "number of an option listed.");
             }
         }
     }
